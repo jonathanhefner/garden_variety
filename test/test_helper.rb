bd1d8c24b3117474e92bd6b96d6d43db03cbba24
Jonathan Hefner <jonathan.hefner@gmail.com>
2017-11-21 13:14:59 -0600

    Fix 'rake test'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require "rails/test_help"
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
+require "rails/test_unit/reporter"
 Rails::TestUnitReporter.executable = 'bin/test'
 
 # Load fixtures from the engine
