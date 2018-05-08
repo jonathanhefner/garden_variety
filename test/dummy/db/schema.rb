d2bbf61e800334ac2537288a10d315e2b019171e
Jonathan Hefner <jonathan.hefner@gmail.com>
2018-05-08 17:11:06 -0500

    cd test/dummy && rails db:migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121194028) do
+ActiveRecord::Schema.define(version: 2017_11_21_194028) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
