0c07e9d7dbb550e78bbe0f7850312f8daef35398
Jonathan Hefner <jonathan@hefner.pro>
2019-01-21 14:41:21 -0600

    Test scaffold generator with namespaced resource
    This test is combined with the "skip conflicting locales" test (as
    the non-namespaced resource and "generate missing locales" tests are)
    to minimize the number of times the Rails scaffold generator must be
    invoked.  The Rails scaffold generator is very slow, and dominates the
    current test suite execution time, even with these tests combined.

--- test/scaffold_generator_test.rb
+++ test/scaffold_generator_test.rb
@@ -7,8 +7,6 @@ class ScaffoldGeneratorTest < Rails::Generators::TestCase
   tests Garden::Generators::ScaffoldGenerator
   destination File.join(__dir__, "tmp")
 
-  LOCALES_FILE = "config/locales/garden_variety.en.yml"
-
   setup do
     prepare_destination
 
@@ -18,34 +16,57 @@ class ScaffoldGeneratorTest < Rails::Generators::TestCase
     end
   end
 
-  def test_generates_scaffold_files
+  teardown :prepare_destination # prevent generated *_test.rb files from confusing `rake test`
+
+  LOCALES_FILE = "config/locales/garden_variety.en.yml"
+
+  def test_generates_scaffold_files_and_missing_locales
     File.delete(File.join(__dir__, "tmp", LOCALES_FILE))
-
-    resource = "fruit"
-    run_generator([resource, "name:string", "--primary-key-type=uuid"])
-
+    generate_scaffold("fruit")
     assert_file LOCALES_FILE
-    assert_file "config/routes.rb", /resources :#{resource.pluralize}/
-    assert_migration "db/migrate/create_#{resource.pluralize}.rb" do |migration|
-      assert_match "create_table :#{resource.pluralize}, id: :uuid", migration
-      assert_match "t.string :name", migration
-    end
-    assert_file "app/models/#{resource}.rb"
-    %w[index show new edit _form].each do |v|
-      assert_file "app/views/#{resource.pluralize}/#{v}.html.erb"
-    end
-    assert_file "app/controllers/#{resource.pluralize}_controller.rb", /^\s+garden_variety$/
-    assert_file "app/policies/#{resource}_policy.rb"
+    assert_scaffold("fruit")
   end
 
-  def test_skips_locales_if_conflict
+  def test_generates_namespaced_scaffold_files_and_skips_conflicting_locales
     # ensure locales file contents will conflict with default locales file
     File.write(File.join(__dir__, "tmp", LOCALES_FILE), "EXPECTED")
 
     # generator will wait for keyboard input if the skip-on-conflict
     # option for copy locales is erroneously omitted
-    Timeout.timeout(15){ run_generator(["vegetable"]) }
+    Timeout.timeout(15){ generate_scaffold("spaced/vegetable") }
 
     assert_file LOCALES_FILE, /EXPECTED/
+    assert_scaffold("spaced/vegetable")
   end
+
+  private
+
+  def generate_scaffold(resource)
+    run_generator([resource, "name:string", "--primary-key-type=uuid"])
+  end
+
+  def assert_scaffold(resource)
+    names = resource.split("/")
+    assert_file "config/routes.rb" do |routes|
+      names[0...-1].each{|name| assert_match "namespace :#{name}", routes }
+      assert_match "resources :#{names[-1].pluralize}", routes
+    end
+
+    table_name = resource.pluralize.tr("/", "_")
+    assert_migration "db/migrate/create_#{table_name}.rb" do |migration|
+      assert_match "create_table :#{table_name}, id: :uuid", migration
+      assert_match "t.string :name", migration
+    end
+
+    assert_file "app/models/#{resource}.rb"
+
+    %w[index show new edit _form].each do |view|
+      assert_file "app/views/#{resource.pluralize}/#{view}.html.erb"
+    end
+
+    assert_file "app/controllers/#{resource.pluralize}_controller.rb", /^\s+garden_variety$/
+
+    assert_file "app/policies/#{resource}_policy.rb"
+  end
+
 end
