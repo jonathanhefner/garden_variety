a67e83a57d13f1c49d9f6f5cdeb6cd4fc6187169
Jonathan Hefner <jonathan@hefner.pro>
2019-01-21 13:49:44 -0600

    Test scaffold generator skips locales on conflict

--- test/scaffold_generator_test.rb
+++ test/scaffold_generator_test.rb
@@ -1,10 +1,13 @@
 require "fileutils"
+require "timeout"
 require "test_helper"
 require "generators/garden/scaffold/scaffold_generator"
 
 class ScaffoldGeneratorTest < Rails::Generators::TestCase
   tests Garden::Generators::ScaffoldGenerator
   destination File.join(__dir__, "tmp")
+
+  LOCALES_FILE = "config/locales/garden_variety.en.yml"
 
   setup do
     prepare_destination
@@ -16,11 +19,12 @@ class ScaffoldGeneratorTest < Rails::Generators::TestCase
   end
 
   def test_generates_scaffold_files
-    assert_file "config/locales/garden_variety.en.yml"
+    File.delete(File.join(__dir__, "tmp", LOCALES_FILE))
 
     resource = "fruit"
     run_generator([resource, "name:string", "--primary-key-type=uuid"])
 
+    assert_file LOCALES_FILE
     assert_file "config/routes.rb", /resources :#{resource.pluralize}/
     assert_migration "db/migrate/create_#{resource.pluralize}.rb" do |migration|
       assert_match "create_table :#{resource.pluralize}, id: :uuid", migration
@@ -34,11 +38,14 @@ class ScaffoldGeneratorTest < Rails::Generators::TestCase
     assert_file "app/policies/#{resource}_policy.rb"
   end
 
-  def test_generates_locales_if_missing
-    locales_file = "config/locales/garden_variety.en.yml"
+  def test_skips_locales_if_conflict
+    # ensure locales file contents will conflict with default locales file
+    File.write(File.join(__dir__, "tmp", LOCALES_FILE), "EXPECTED")
 
-    File.delete(File.join(__dir__, "tmp", locales_file))
-    run_generator(["vegetable"])
-    assert_file locales_file
+    # generator will wait for keyboard input if the skip-on-conflict
+    # option for copy locales is erroneously omitted
+    Timeout.timeout(15){ run_generator(["vegetable"]) }
+
+    assert_file LOCALES_FILE, /EXPECTED/
   end
 end
