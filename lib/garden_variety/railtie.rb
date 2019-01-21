194321d78d34feb50d7a94e658781f0af7901378
Jonathan Hefner <jonathan@hefner.pro>
2019-01-21 12:52:23 -0600

    Modernize railtie

--- lib/garden_variety/railtie.rb
+++ lib/garden_variety/railtie.rb
@@ -1,6 +1,6 @@
 module GardenVariety
   # @!visibility private
-  class Railtie < Rails::Railtie
+  class Railtie < ::Rails::Railtie
     # Render 404 on Pundit::NotAuthorizedError in production.  (Helpful
     # error pages will still be shown in development.)  Code 404 is used
     # because it is more discreet than 403, because it is explicitly
@@ -8,17 +8,10 @@ module GardenVariety
     # and because Rails includes a default 404 page, but not a 403 page.
     config.action_dispatch.rescue_responses["Pundit::NotAuthorizedError"] ||= :not_found
 
-    initializer "garden_variety.stub_current_user" do |app|
+    initializer "garden_variety" do |app|
       ActiveSupport.on_load :action_controller do
-        unless ActionController::Base.instance_methods.include?(:current_user)
-          ActionController::Base.send :include, GardenVariety::CurrentUserStub
-        end
-      end
-    end
-
-    initializer "garden_variety.extend_action_controller" do |app|
-      ActiveSupport.on_load :action_controller do
-        ActionController::Base.send :include, GardenVariety::Controller
+        include GardenVariety::CurrentUserStub unless instance_methods.include?(:current_user)
+        include GardenVariety::Controller
       end
     end
   end
