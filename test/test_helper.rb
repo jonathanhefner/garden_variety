55af1a2cd91468a35a8c795014f6e9133acd88f6
Jonathan Hefner <jonathan@hefner.pro>
2019-09-10 14:24:45 -0500

    Add missing test configuration
    This configuration code is traditionally included in a newly generated
    Rails plugins, but was excluded from plugins generated via Rails 5.1
    (see rails/rails@9a80f52, reverted by rails/rails@cd0283e).  Without
    this configuration, tests are run in the `development` environment.
    Previously, that did not cause any problems.  However, Rails 6.0
    includes a new guard against DNS rebinding attacks that is activated in
    the `development` environment by default (see rails/rails@07ec806).
    This guard, by default, blocks any requests that specify a host other
    than some variant of "localhost".  As a consequence, requests made via
    `ActionDispatch::IntegrationTest`, which uses "www.example.com" as the
    default request host, would always return response code 403.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
 Bundler.load.setup(:default, :development) # exclude :talent_scout group
 
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+
 require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
